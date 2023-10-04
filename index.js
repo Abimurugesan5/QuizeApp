d57005eecd92e1924524b2cf2ef7d39d8d5d311c
Abi Murugesan <abimurugesan58@gmail.com>
2023-10-04 19:21:02 +0530

    timer setting

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ let score = 0;
 
 const questionText = document.getElementById("question-text");
 const optionsContainer = document.getElementById("options-container");
-const nextButton = document.getElementById("next-button");
+const nextButtons = document.getElementById("next-button");
 
 function showQuestion() {
     const question = questions[currentQuestion];
@@ -58,4 +58,46 @@ function endQuiz() {
 }
 
 showQuestion();
-nextButton.addEventListener("click", () => showQuestion());
+nextButtons.addEventListener("click", () => showQuestion());
+const timerCountdown = 10;
+let timer = timerCountdown;
+let timerInterval;
+
+const timerElement = document.getElementById("timer-count");
+const nextButton = document.getElementById("next-button");
+
+// Function to start the timer
+function startTimer() {
+    timer = timerCountdown;
+    updateTimerDisplay();
+    timerInterval = setInterval(updateTimer, 1000);
+}
+
+
+function updateTimerDisplay() {
+    timerElement.textContent = timer;
+}
+
+
+function updateTimer() {
+    if (timer > 0) {
+        timer--;
+        updateTimerDisplay();
+    } else {
+        clearInterval(timerInterval);
+       
+        alert("Time's up! Click Next to proceed.");
+    }
+}
+
+
+startTimer();
+
+nextButton.addEventListener("click", () => {
+    clearInterval(timerInterval);
+    startTimer();
+    const questionText = document.getElementById("question-text");
+    const currentQuestion = parseInt(questionText.textContent.match(/\d+/));
+    questionText.textContent = `Question ${currentQuestion + 1}: What is the capital of France?`;
+});
+
